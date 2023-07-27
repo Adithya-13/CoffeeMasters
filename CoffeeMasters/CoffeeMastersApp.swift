c83aff3b05817c45164485328d37dc9b4d77407d
Adithya Firmansyah Putra <aditputrafirmansyah@gmail.com>
2023-07-27 22:45:06 +0700

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Adithya Firmansyah Putra on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
